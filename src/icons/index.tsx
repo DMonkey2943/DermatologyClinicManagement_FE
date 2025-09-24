56990a12ccb764c7d80c99256a6247a06f57ea2a
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-09-24 08:54:17 +0700

    style: add capsule-pill and stethoscope icon

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -51,6 +51,8 @@ import HorizontaLDots from "./horizontal-dots.svg";
 import ChatIcon from "./chat.svg";
 import MoreDotIcon from "./more-dot.svg";
 import BellIcon from "./bell.svg";
+import MedicationIcon from "./capsule-pill.svg";
+import StethoscopeIcon from "./stethoscope.svg";
 
 export {
   DownloadIcon,
@@ -106,4 +108,6 @@ export {
   HorizontaLDots,
   ChevronUpIcon,
   ChatIcon,
+  MedicationIcon,
+  StethoscopeIcon,
 };
