ee114e65747ebdf6a14c58dd1a71340a2deeafb7
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-09-26 08:58:32 +0700

    chore: add type for defaultDate in date-picker component

--- src/components/form/date-picker.tsx
+++ src/components/form/date-picker.tsx
@@ -10,7 +10,7 @@ type PropsType = {
   id: string;
   mode?: "single" | "multiple" | "range" | "time";
   onChange?: Hook | Hook[];
-  defaultDate?: DateOption;
+  defaultDate?: DateOption | null | string;
   label?: string;
   placeholder?: string;
 };
