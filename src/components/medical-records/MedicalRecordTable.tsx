6ebc10bab03746de4512c6ad31830d4f851b7aee
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-10-18 16:55:07 +0700

    feat(MedicalRecords): display status field and view detail button

--- src/components/medical-records/MedicalRecordTable.tsx
+++ src/components/medical-records/MedicalRecordTable.tsx
@@ -8,9 +8,9 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-// import Badge from '@/components/ui/badge/Badge';
+import Badge from '@/components/ui/badge/Badge';
 import Button from '@/components/ui/button/Button';
-// import Link from 'next/link';
+import Link from 'next/link';
 import { MedicalRecordDataType } from '@/schemaValidations/medicalRecord.schema';
 import { formatDateTime } from '@/lib/utils';
 
@@ -19,6 +19,15 @@ interface MedicalRecordTableProps {
 }
 
 export default function MedicalRecordTable({ medical_records }: MedicalRecordTableProps) {
+  const getStatusColor = (status: string|null) => {
+    switch (status) {
+      case 'IN_PROGRESS':
+        return 'warning';
+      case 'COMPLETED':
+        return 'success';
+    }
+  };
+
   return (
     <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
       <div className="max-w-full overflow-x-auto">
@@ -49,6 +58,12 @@ export default function MedicalRecordTable({ medical_records }: MedicalRecordTab
                   isHeader
                   className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
                 >
+                  Trạng thái
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
                   Tùy chọn
                 </TableCell>
               </TableRow>
@@ -67,9 +82,15 @@ export default function MedicalRecordTable({ medical_records }: MedicalRecordTab
                   <TableCell className="px-5 py-4 text-start text-theme-sm text-gray-800 dark:text-white/90">
                     {medical_record.doctor.full_name}
                   </TableCell>
+                  <TableCell className="px-5 py-4 text-start text-theme-sm text-gray-800 dark:text-white/90">
+                    <Badge size="sm" color={getStatusColor(medical_record.status)}>{medical_record.status}</Badge>
+                  </TableCell>
                   <TableCell className="px-5 py-4 text-start text-theme-xs text-gray-500 dark:text-gray-400">
                     <div className="flex gap-2">
-                      <Button size="sm" onClick={() =>alert('Chuyển trang chi tiết thông tin bệnh nhân')}>Xem</Button>
+                      {/* <Button size="sm" onClick={() => alert('Chuyển trang chi tiết thông tin phiên khám')}>Xem</Button> */}
+                      <Link href={`/medical-records/${medical_record.id}`}>
+                        <Button size="sm">Xem</Button>
+                      </Link>
                       {/* <Link href={`/medical_records/${medical_record.id}`}>
                         <Button size="sm">Xem</Button>
                       </Link>
