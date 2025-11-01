01a384c8dd8b00cb9475535459306eb4df2f795b
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-11-01 22:52:59 +0700

    chore: format date from date string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,6 +24,16 @@ export const formatDateTime = (isoString: string) => {
   return `${day}/${month}/${year} - ${hours}:${minutes}`;
 };
 
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const year = date.getFullYear();
+  // const hours = String(date.getHours()).padStart(2, '0');
+  // const minutes = String(date.getMinutes()).padStart(2, '0');
+  return `${day}/${month}/${year}`;
+};
+
 export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat('vi-VN', {
     style: 'currency',
