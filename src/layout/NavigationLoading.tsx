36c0eb329d64c14fa3568801a2274178256d06bb
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-11-11 21:31:47 +0700

    feat: show progress bar when navigating between pages

---
+++ src/layout/NavigationLoading.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+export default function NavigationLoading() {
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Khi pathname thay đổi => kết thúc loading
+    NProgress.done(); // kết thúc progress khi route load xong
+    setIsLoading(false);
+  }, [pathname]);
+
+  useEffect(() => {
+    // Khi bắt đầu chuyển route => bật loading
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      if (target.closest("a")) {
+        NProgress.start();
+        setIsLoading(true);
+      }
+    };
+
+    window.addEventListener("click", handleClick);
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+//   if (!isLoading) return null;
+
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center bg-black/30 z-[999999]">
+//       <div className="h-10 w-10 border-4 border-t-transparent border-white rounded-full animate-spin" />
+//     </div>
+//   );
+    return null;
+}
